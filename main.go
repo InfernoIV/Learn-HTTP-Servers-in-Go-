4912f286357b3d1210e22400e1ec70f10dcff4c1
Joost <github@joostnieuweboer.nl>
2025-11-01 15:30:18 +0000

    Chapter 1, Lesson 11

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"net/http"
+	"//io"
 )
 
 func main() {
@@ -11,8 +12,8 @@ func main() {
 	//Use the http.NewServeMux's .Handle() method to add a handler for the root path (/).
 	//Use a standard http.FileServer as the handler
 	//Use http.Dir to convert a filepath (in our case a dot: . which indicates the current directory) to a directory for the http.FileServer.
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-
+	mux.Handle("/app/", http.StripPrefix("/app/",http.FileServer(http.Dir("."))));
+	mux.HandleFunc("/healthz", readiness_endpoint)
 	//Create a new http.Server struct.
 	server := http.Server{}
 	//Use the new "ServeMux" as the server's handler
@@ -23,4 +24,14 @@ func main() {
 
 	//Use the server's ListenAndServe method to start the server
 	server.ListenAndServe()
+}
+
+func readiness_endpoint(w http.ResponseWriter, r *http.Request){
+	//Write the Content-Type header
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	//Write the status code using w.WriteHeader
+	w.WriteHeader(http.StatusOK)
+	//Write the body text using w.Write
+	//io.WriteString(w, "OK");
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
